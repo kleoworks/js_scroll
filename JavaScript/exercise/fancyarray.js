8fd6cd1f6f2311fb8a1b0517ddcd7b41346b8e03
kleoworks <leo@kleoworks.com>
2016-12-20 12:16:50 -0600

    cleaning directories

--- JavaScript/exercise/fancyarray.js
+++
@@ -1,9 +0,0 @@
-function nameArray (a,b,c,d)
-{
-    var arr = [a,b,c,d];
-    for (var i = 0; i < arr.length; i++) {
-        console.log( i + " -> " + arr[i] );
-    }
-}
-
-nameArray ("->", "=>", "::", "----")
--- aboutMe.md
+++
@@ -1,3 +0,0 @@
-Name: Leonard Kang
-Why I am here: Coding Dojo is cool
-Favorite Book Series: Cat in the Hat
